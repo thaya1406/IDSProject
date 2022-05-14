8054085d061ecf4ed4de0c824b6f2a251739c867
samueldouni <92864951+samueldouni@users.noreply.github.com>
2022-05-15 00:10:06 +0800

    Rough Idea of how User inputs Location

---
+++ LocationInputPlot.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+library(sf)
+library(rnaturalearth)
+
+input_read <- readline(prompt = "Enter Name: ") ##Need to execute this line first
+
+##Then only these statement is alright
+if (is.character(input_read)) {
+
+worldmap <- ne_countries(scale = 'medium', type = 'map_units',
+                         returnclass = 'sf')
+# have a look at these two columns only
+head(worldmap[c('name', 'continent')])
+
+ggplot() + geom_sf(data = worldmap) + theme_bw()
+
+location <- worldmap[worldmap$name == input_read,]
+ggplot() + geom_sf(data = location) + theme_bw()
+} else {
+  print("Invalid keyword!")
+}

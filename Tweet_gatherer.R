94f985c8a12f25e55f8e5a724dc52d3d9003c170
samueldouni <92864951+samueldouni@users.noreply.github.com>
2022-05-28 23:05:33 +0800

    Update Tweet_gatherer.R

--- Tweet_gatherer.R
+++ Tweet_gatherer.R
@@ -1,4 +1,4 @@
-library(gathertweet)
+library(gathertweet) ##Need to download thru his repository not thru the install.packages thingy
 gathertweet_search("najib") ##You change this term, I don't know how to 'connect' it with the input textbox :(
 gathertweet_simplify()
 

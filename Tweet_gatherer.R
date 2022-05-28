23bb3dc095eb30027bb656694a0ccd2161e0347d
samueldouni <92864951+samueldouni@users.noreply.github.com>
2022-05-28 23:06:50 +0800

    Update Tweet_gatherer.R

--- Tweet_gatherer.R
+++ Tweet_gatherer.R
@@ -1,4 +1,5 @@
 library(gathertweet) ##Need to download thru his repository not thru the install.packages thingy
 gathertweet_search("najib") ##You change this term, I don't know how to 'connect' it with the input textbox :(
-gathertweet_simplify()
 
+## THINGS TO TAKE NOTE
+## Set the working directory to data/ because when you run the gathertweet_search("   ") line, it'll produce a tweets.rds file needed for the Tweet Wall
